792472d7642cad034525f650788f9120c27e4f44
Tore Hald Simonsen <toreh.simonsen@live.dk>
2020-03-25 13:57:11 +0100

    First rogueLike begun

---
+++ Rougelite/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using Random = UnityEngine.Random;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    [Serializable]
+    public class Count{
+        public int maximum;
+        public int minimum;
+
+        public Count(int min, int max)
+        {
+            minimum = min;
+            maximum = max;
+        }
+    }
+
+    public int coloumns = 8;
+    public int rows = 8;
+    public Count wallCount = new Count(5, 9);
+    public Count foodCount = new Count(1, 5);
+    public GameObject exit;
+    public GameObject[] floorTiles;
+    public GameObject[] wallTiles;
+    public GameObject[] foodTiles;
+    public GameObject[] enemyTiles;
+    public GameObject[] outerWallTiles;
+
+    private Transform boardHolder;
+    private List<Vector3> gridPositions = new List<Vector3>;
+
+    void InitializeList()
+    {
+        gridPositions.Clear();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
